45173526c588f039849ae06f449df082434fbd80
darkynt <matt.cavanagh@digital.cabinet-office.gov.uk>
2026-01-21 10:05:00 +0000

    chore: initial repo skeleton

---
+++ lib/default-props/lambda-function.ts
@@ -0,0 +1,6 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export default {
+  runtime: lambda.Runtime.NODEJS_24_X,
+  handler: 'index.handler'
+}
---
+++ lib/helpers/env-defaults.ts
@@ -0,0 +1,16 @@
+import {camelCase} from 'lodash/fp';
+
+type optionsDictionary = {[key: string]: string | undefined}
+
+const PLATFORM_CDK_ENV_PREFIX = 'PCDK_';
+
+const env = process.env ?? {} as optionsDictionary;
+
+export const getDefaults = (options = {} as optionsDictionary) => Object.fromEntries(
+  Object.entries(options)
+    .filter(([k, v]) => (k.startsWith(PLATFORM_CDK_ENV_PREFIX)))
+    .map(([k, v]) => ([k.split(PLATFORM_CDK_ENV_PREFIX)[1], v]))
+    .map(([k = '', v]) => ([camelCase(k), v]))
+);
+
+export default getDefaults(env);
